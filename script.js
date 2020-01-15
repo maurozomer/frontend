cee77ab109f707fed75863dc53a01378c814c309
maurozomer <59627762+maurozomer@users.noreply.github.com>
2020-01-15 15:39:20 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+var button = document.querySelector("button");
+var element = document.querySelector("nav");
+
+button.addEventListener("click", function() {
+  element.classList.toggle("toggle");
+});
